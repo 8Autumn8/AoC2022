5a4e8804403d199a650b5542c4551087f0dc204e
8Autumn8 <66743952+8Autumn8@users.noreply.github.com>
2023-03-19 23:17:58 -0700

    Update Day_3.py

--- Day_3.py
+++ Day_3.py
@@ -17,9 +17,8 @@ def starOne():
 
 def starTwo():
   sum = 0
-  i = 0
   compartments = rawData.split("\n")
-  while (i < len(compartments)-2):
+  for i in range(0, len(compartments), 3):
     res_first, res_second, res_third = compartments[i], compartments[i+1], compartments[i+2]
     s1_chars, s2_chars, s3_chars  = set(res_first), set(res_second), set(res_third)
     ascii = ord(list(s1_chars.intersection(s2_chars, s3_chars))[0])
@@ -27,7 +26,6 @@ def starTwo():
     #converting ascii to values a-z to 1-26 and A-Z to 27-52
     if ascii > 96: sum += ascii-96
     else: sum += ascii-38
-    i+=3
   return sum
 
 print(starOne())
