b1c972fbb4391e356775362abb0f3e509859f758
8Autumn8 <66743952+8Autumn8@users.noreply.github.com>
2023-03-19 23:11:11 -0700

    Update Day_3.py
    I was bothered on the readability of my code

--- Day_3.py
+++ Day_3.py
@@ -8,11 +8,11 @@ def starOne():
   for item in compartments:
     res_first, res_second = item[:len(item)//2], item[len(item)//2:]
     s1_chars, s2_chars  = set(res_first), set(res_second)
-    ascii = ord(sorted(s1_chars.intersection(s2_chars))[0])
+    ascii = ord(list(s1_chars.intersection(s2_chars))[0])
 
     #converting ascii to values a-z to 1-26 and A-Z to 27-52
     if ascii > 96: sum += ascii-96
-    else:sum += ascii-38
+    else: sum += ascii-38
   return sum
 
 def starTwo():
@@ -22,11 +22,11 @@ def starTwo():
   while (i < len(compartments)-2):
     res_first, res_second, res_third = compartments[i], compartments[i+1], compartments[i+2]
     s1_chars, s2_chars, s3_chars  = set(res_first), set(res_second), set(res_third)
-    ascii = ord(sorted(s1_chars.intersection(s2_chars, s3_chars))[0])
     
+    ascii = ord(list(s1_chars.intersection(s2_chars, s3_chars))[0])
     #converting ascii to values a-z to 1-26 and A-Z to 27-52
     if ascii > 96: sum += ascii-96
-    else:sum += ascii-38
+    else: sum += ascii-38
     i+=3
   return sum
 
